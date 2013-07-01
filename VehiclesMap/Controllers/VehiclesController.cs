ca2ac2604d04c6be98d411c43e51b024dbe9564b
Andection <andection@gmail.com>
2013-07-01 21:47:23 +0600

    [*] add first page and controller

---
+++ VehiclesMap/Controllers/VehiclesController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace VehiclesMap.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
