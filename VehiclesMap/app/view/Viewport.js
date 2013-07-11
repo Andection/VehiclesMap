365c5b1db76dd0de95cfa312da4c83d1bcb632fa
Andection <andection@gmail.com>
2013-07-11 07:04:22 +0600

    add map

--- VehiclesMap/app/view/Viewport.js
+++ VehiclesMap/app/view/Viewport.js
@@ -1,16 +1,18 @@
+Ext.Loader.setConfig({ enabled: true });
+Ext.Loader.setPath('Ext.ux', '../ux');
 
 Ext.define('VehiclesMap.view.Viewport', {
     extend: 'Ext.container.Viewport',
     layout: 'fit',
-    
+
     requires: [
-        'VehiclesMap.view.ManageMap'
-      //  'VehiclesMap.lib.GMapPanel'
+        'VehiclesMap.view.ManageMap',
+        'VehiclesMap.view.GMapPanel'
     ],
-    
+
     initComponent: function() {
         this.items = {
-               dockedItems: [{
+            dockedItems: [{
                 dock: 'left',
                 xtype: 'toolbar',
                 items: [{
@@ -18,17 +20,30 @@ Ext.define('VehiclesMap.view.Viewport', {
                     width: 100
                 }]
             }],
-            //items: [{
-            //    xtype: 'gmappanel',
-            //    zoomLevel: 14,
-            //    gmapType: 'map',
-            //    mapConfOpts: ['enableScrollWheelZoom', 'enableDoubleClickZoom', 'enableDragging'],
-            //    mapControls: ['GSmallMapControl', 'GMapTypeControl', 'NonExistantControl']
-            //}],
-            layout: {
-                type: 'hbox',
-                align: 'stretch'
-            },
+            items: [
+                {
+                    xtype: 'gmappanel',
+                    center: {
+                        geoCodeAddr: '4 Yawkey Way, Boston, MA, 02215-3409, USA',
+                        marker: { title: 'Fenway Park' }
+                    },
+                    markers: [{
+                            lat: 42.339641,
+                            lng: -71.094224,
+                            title: 'Boston Museum of Fine Arts',
+                            listeners: {
+                                click: function(e) {
+                                    Ext.Msg.alert('It\'s fine', 'and it\'s art.');
+                                }
+                            }
+                        }, {
+                            lat: 42.339419,
+                            lng: -71.09077,
+                            title: 'Northeastern University'
+                        }],
+                    layout: 'fit',
+                }],
+            layout: 'fit'
         };
         this.callParent();
     }
